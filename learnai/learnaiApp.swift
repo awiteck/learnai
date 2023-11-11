46fe0f338e00ba784efe9045aeeb291f2de53ee7
aidanwiteck <59320558+aidanwiteck@users.noreply.github.com>
2023-11-10 21:10:24 -0500

    first commit

---
+++ learnai/learnaiApp.swift
@@ -0,0 +1,17 @@
+//
+//  learnaiApp.swift
+//  learnai
+//
+//  Created by Aidan Witeck on 11/10/23.
+//
+
+import SwiftUI
+
+@main
+struct learnaiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
