9c74b4e61c81f55ed7656249bb49cda67620cb51
Kristoffer Selberg <kselberg@dynamic-oit-vapornet100-10-8-45-212.princeton.edu>
2023-11-11 17:27:44 -0500

    fixed register endpoint

--- apiserver/index.js
+++ apiserver/index.js
@@ -1,14 +1,22 @@
 // index.js
-require("dotenv").config();
 const express = require("express");
 const { Pool } = require("pg");
 
+// Load environment variables from .env file if not in production
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 // Firebase
 const admin = require("firebase-admin");
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+  const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+} else {
+  console.warn("Missing environment variable for FIREBASE_SERVICE_ACCOUNT");
+}
 
 // Verify Firebase token and get the user ID
 const getFirebaseUserId = async (idToken) => {
@@ -51,20 +59,24 @@ app.get("/facts/:topic", async (req, res) => {
   }
 });
 
-app.get('/facts/unviewed/interests', async (req, res) => {
+app.get("/facts/unviewed/interests", async (req, res) => {
   const userUid = req.user.uid; // Assuming req.user.uid is set after authentication
   const limit = 5; // The batch size of unviewed facts to retrieve
 
   try {
     // Get the user_id from the users table using the firebase_uid
-    const userResult = await pool.query('SELECT user_id FROM users WHERE firebase_uid = $1', [userUid]);
+    const userResult = await pool.query(
+      "SELECT user_id FROM users WHERE firebase_uid = $1",
+      [userUid]
+    );
     if (userResult.rows.length === 0) {
-      return res.status(404).send('User not found.');
+      return res.status(404).send("User not found.");
     }
     const userId = userResult.rows[0].user_id;
 
     // Select 5 unviewed facts whose topics are within the user's topics of interest
-    const factsResult = await pool.query(`
+    const factsResult = await pool.query(
+      `
       SELECT f.fact_id, f.fact_title, f.fact_text, f.topic_id
       FROM facts f
       INNER JOIN users_topics ut ON f.topic_id = ut.topic_id
@@ -73,63 +85,72 @@ app.get('/facts/unviewed/interests', async (req, res) => {
       GROUP BY f.fact_id, f.fact_title, f.fact_text, f.topic_id
       ORDER BY f.fact_id
       LIMIT $2;
-    `, [userId, limit]);
+    `,
+      [userId, limit]
+    );
 
     // Respond with the facts
     res.json(factsResult.rows);
   } catch (error) {
-    console.error('Error retrieving unviewed facts within user interests:', error);
-    res.status(500).send('Internal server error.');
+    console.error(
+      "Error retrieving unviewed facts within user interests:",
+      error
+    );
+    res.status(500).send("Internal server error.");
   }
 });
 
-
-app.post('/facts/:factId/upvote', async (req, res) => {
+app.post("/facts/:factId/upvote", async (req, res) => {
   const { factId } = req.params;
   const userUid = req.user.uid; // Assuming req.user.uid is set after authentication
 
   try {
     // Insert the upvote record into the users_upvoted table
-    const result = await pool.query(`
+    const result = await pool.query(
+      `
       INSERT INTO users_upvoted (user_id, fact_id)
       SELECT u.user_id, $1 FROM users u WHERE u.firebase_uid = $2
       ON CONFLICT (user_id, fact_id) DO NOTHING;
-    `, [factId, userUid]);
+    `,
+      [factId, userUid]
+    );
 
     if (result.rowCount === 0) {
-      return res.status(409).send('Fact already upvoted by the user.');
+      return res.status(409).send("Fact already upvoted by the user.");
     }
 
-    res.status(200).send('Fact upvoted successfully.');
+    res.status(200).send("Fact upvoted successfully.");
   } catch (error) {
-    console.error('Error upvoting fact:', error);
-    res.status(500).send('Internal server error.');
+    console.error("Error upvoting fact:", error);
+    res.status(500).send("Internal server error.");
   }
 });
 
-app.post('/facts/:factId/downvote', async (req, res) => {
+app.post("/facts/:factId/downvote", async (req, res) => {
   const { factId } = req.params;
   const userUid = req.user.uid; // Assuming req.user.uid is set after authentication
 
   try {
     // Insert the downvote record into the users_downvoted table
-    const result = await pool.query(`
+    const result = await pool.query(
+      `
       INSERT INTO users_downvoted (user_id, fact_id)
       SELECT u.user_id, $1 FROM users u WHERE u.firebase_uid = $2
       ON CONFLICT (user_id, fact_id) DO NOTHING;
-    `, [factId, userUid]);
+    `,
+      [factId, userUid]
+    );
 
     if (result.rowCount === 0) {
-      return res.status(409).send('Fact already downvoted by the user.');
+      return res.status(409).send("Fact already downvoted by the user.");
     }
 
-    res.status(200).send('Fact downvoted successfully.');
+    res.status(200).send("Fact downvoted successfully.");
   } catch (error) {
-    console.error('Error downvoting fact:', error);
-    res.status(500).send('Internal server error.');
+    console.error("Error downvoting fact:", error);
+    res.status(500).send("Internal server error.");
   }
 });
-
 
 // User Registration
 app.post("/register", async (req, res) => {
@@ -143,10 +164,10 @@ app.post("/register", async (req, res) => {
     );
 
     // Respond with the created user's ID from your database
-    res.status(201).json({ userId: queryResult.rows[0].id });
+    res.status(201).json({ user_id: queryResult.rows[0].user_id });
   } catch (err) {
     console.error(err);
-    if (err.code === "auth/email-already-exists") {
+    if (err.code === "23505") {
       res.status(400).send("Email already exists.");
     } else {
       res.status(500).send("Internal server error.");
