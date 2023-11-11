9c077a20db7a832233e58885d1c4fbc5abff3037
Kristoffer Selberg <kselberg@dynamic-oit-vapornet100-10-8-45-212.princeton.edu>
2023-11-11 13:58:47 -0500

    process service account with env var

--- apiserver/index.js
+++ apiserver/index.js
@@ -5,7 +5,7 @@ const { Pool } = require("pg");
 
 // Firebase
 var admin = require("firebase-admin");
-var serviceAccount = require("./firebase-service-account.json");
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
