f2e3e6abf2b44cef7ced52247da9d117b9a57c39
Kristoffer Selberg <kselberg@dynamic-oit-vapornet100-10-8-45-212.princeton.edu>
2023-11-11 16:53:16 -0500

    added firebase and /users/topics post endpoint

--- apiserver/index.js
+++ apiserver/index.js
@@ -3,11 +3,30 @@ require("dotenv").config();
 const express = require("express");
 const { Pool } = require("pg");
 
+// Firebase
+const admin = require("firebase-admin");
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+// Verify Firebase token and get the user ID
+const getFirebaseUserId = async (idToken) => {
+  try {
+    const decodedToken = await admin.auth().verifyIdToken(idToken);
+    return decodedToken.uid; // This UID should correspond to `firebase_uid` in DB
+  } catch (error) {
+    console.error("Error verifying Firebase token:", error);
+    throw error;
+  }
+};
+
 const app = express();
 const port = process.env.PORT || 3000;
+const connectionString = process.env.DB_URL;
 
 const pool = new Pool({
-  connectionString: process.env.DB_URL,
+  connectionString: connectionString,
 });
 
 app.use(express.json());
@@ -21,19 +40,16 @@ app.get("/", (req, res) => {
 app.get("/facts/:topic", async (req, res) => {
   try {
     const { topic } = req.params;
-    const result = await pool.query("SELECT facts.* FROM facts INNER JOIN topics ON facts.topic_id = topics.topic_id WHERE topics.topic_name = $1;",
-    [
-      topic,
-    ]);
+    const result = await pool.query(
+      "SELECT facts.* FROM facts INNER JOIN topics ON facts.topic_id = topics.topic_id WHERE topics.topic_name = $1;",
+      [topic]
+    );
     res.json(result.rows);
   } catch (err) {
     console.error(err);
     res.status(500).send("Server error");
   }
 });
-
-
-
 
 // User Registration
 app.post("/register", async (req, res) => {
@@ -52,6 +68,61 @@ app.post("/register", async (req, res) => {
     console.error(err);
     if (err.code === "auth/email-already-exists") {
       res.status(400).send("Email already exists.");
+    } else {
+      res.status(500).send("Internal server error.");
+    }
+  }
+});
+
+app.post("/users/topics", async (req, res) => {
+  const { idToken, topic_id } = req.body; // Expect the Firebase ID token and topic ID in the request body
+
+  try {
+    // Verify the ID token and get the firebase_uid
+    const firebase_uid = await getFirebaseUserId(idToken);
+
+    // Get the user_id from the users table using firebase_uid
+    const userResult = await pool.query(
+      "SELECT user_id FROM users WHERE firebase_uid = $1",
+      [firebase_uid]
+    );
+    if (userResult.rowCount === 0) {
+      return res.status(404).send("User not found.");
+    }
+    const user_id = userResult.rows[0].user_id;
+
+    // Check if the topic exists
+    const topicExists = await pool.query(
+      "SELECT 1 FROM topics WHERE topic_id = $1",
+      [topic_id]
+    );
+    if (topicExists.rowCount === 0) {
+      return res.status(404).send("Topic not found.");
+    }
+
+    // Check if the association already exists
+    const existingAssociation = await pool.query(
+      "SELECT 1 FROM users_topics WHERE user_id = $1 AND topic_id = $2",
+      [user_id, topic_id]
+    );
+    if (existingAssociation.rowCount > 0) {
+      return res.status(409).send("User already associated with this topic.");
+    }
+
+    // Insert the user_id and topic_id into the users_topics table
+    const insertResult = await pool.query(
+      "INSERT INTO users_topics (user_id, topic_id) VALUES ($1, $2)",
+      [user_id, topic_id]
+    );
+
+    // Respond with success message
+    res.status(201).send("Topic associated with user successfully.");
+  } catch (err) {
+    console.error(err);
+    if (err.code === "auth/id-token-revoked") {
+      res.status(401).send("Firebase ID token has been revoked.");
+    } else if (err.code === "auth/id-token-expired") {
+      res.status(401).send("Firebase ID token has expired.");
     } else {
       res.status(500).send("Internal server error.");
     }
