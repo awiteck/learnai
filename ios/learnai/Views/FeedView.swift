802bb78333a2b998089dfe02b895df467b095a82
Daniel Hu <dh37@princeton.edu>
2023-11-11 20:30:48 -0500

    txt to speech

--- ios/learnai/Views/FeedView.swift
+++ ios/learnai/Views/FeedView.swift
@@ -7,10 +7,12 @@
 
 import Foundation
 import SwiftUI
+import AVKit
 
 struct FeedView: View {
     @State private var stories = [Story]()
     @State private var currentIndex = 0
+    let speechSynthesizer = AVSpeechSynthesizer()
     
     var body: some View {
         TabView(selection: $currentIndex) {
@@ -22,11 +24,16 @@ struct FeedView: View {
                             loadMoreStories()
                         }
                     }
+                    .onChange(of: currentIndex) { newIndex in
+                        print("triggering")
+                        readStory(index: newIndex)
+                    }
             }
         }
         .tabViewStyle(.page(indexDisplayMode: .never))
         .onAppear {
             loadMoreStories()
+            readStory(index: 0)
         }
     }
     
@@ -38,5 +45,25 @@ struct FeedView: View {
             Story(title: "The oud", content: "The oud, a stringed instrument resembling a lute, is considered one of the most important in Middle Eastern music. Its origins can be traced back to the ancient civilizations of Mesopotamia over 3,500 years ago. The oud has played a central role in the musical traditions of the Arabian Peninsula, Persia, and the Levant, influencing musical styles and instruments across the Mediterranean, including the European lute. With a deep, resonant sound and a rich history, the oud is not just an instrument but a symbol of the cultural and musical heritage of the Middle East.", followUpQuestion: "Question 2", categories: ["Music", "Middle East"])
         ]
         stories.append(contentsOf: newStories)
+    }
+
+    func readStory(index: Int) {
+        guard !stories.isEmpty else {
+            print("The stories array is empty.")
+            return
+        }
+        
+        let firstStoryContents = stories[index].content
+        
+        // Stop any current speech
+        if speechSynthesizer.isSpeaking {
+            speechSynthesizer.stopSpeaking(at: .immediate)
+        }
+
+        // Create an utterance with the text you want to speak
+        let utterance = AVSpeechUtterance(string: firstStoryContents)
+        
+        utterance.voice = AVSpeechSynthesisVoice(identifier: "com.apple.ttsbundle.siri_male_en-GB_compact")
+        speechSynthesizer.speak(utterance)
     }
 }
